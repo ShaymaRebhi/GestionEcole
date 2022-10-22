8759a14cabbf57dc737c58f2202bf4fa301a5eaf
Shayma REBHI <shayma.rebhi@esprit.tn>
2022-10-22 13:27:37 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -25,39 +25,39 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/tables', function () {
     return view('layouts.tables');
 })->name('tables');
+Route::get('/coursList', function () {
+    return view('Components.Cours.coursList');
+})->name('coursList');
+
+Route::get('/clubsList', function () {
+    return view('Components.Club.clubsList');
+})->name('clubsList');
+
+Route::get('/eventsList', function () {
+    return view('Components.Event.eventsList');
+})->name('eventsList');
+
+Route::get('/moduleList', function () {
+    return view('Components.Module.moduleList');
+})->name('moduleList');
 
 
+Route::get('/formationsexternesList', function () {
+    return view('Components.Formationsexternes.formationexternesList');
+})->name('formationsexternesList');
 
 
+Route::get('/formationsinternesList', function () {
+    return view('Components.Formationsinternes.formationsinternesList');
+})->name('formationsinternesList');
 
+Route::get('/centreformationList', function () {
+    return view('Components.Centreformation.centreformationList');
+})->name('centreformationList');
+Route::resource('FormationExterne', FormationExterneController::class);
+Route::resource('FormationInterne', FormationInterneController::class);
+Route::resource('CentreFormation', CentreFormationController::class);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Categories
 Route::group(['prefix' => 'categories' ,'as' => 'categories.' ],function () {
     Route::get('/', [CategoryController::class, 'index'])->name('index');
 });
